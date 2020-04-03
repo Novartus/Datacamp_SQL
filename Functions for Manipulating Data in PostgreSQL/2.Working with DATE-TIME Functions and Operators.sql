6a349f73a908b7cf0be8a53ae889d908b7a86d39
Abhee Hudani <hudaniabhee@gmail.com>
2020-04-04 03:18:54 +0530

    Chapter 2 Completed

--- Functions for Manipulating Data in PostgreSQL/2.Working with DATE-TIME Functions and Operators.sql
+++ Functions for Manipulating Data in PostgreSQL/2.Working with DATE-TIME Functions and Operators.sql
@@ -1,5 +1,5 @@
-# Adding and subtracting date and time values
-### (1)
+-- Adding and subtracting date and time values
+-- (1)
 SELECT f.title, f.rental_duration,
     r.return_date - r.rental_date AS days_rented
 FROM film AS f
@@ -7,7 +7,7 @@ FROM film AS f
      INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
 ORDER BY f.title;
 
-### (2)
+-- (2)
 SELECT f.title, f.rental_duration,
 	AGE(r.return_date, r.rental_date) AS days_rented
 FROM film AS f
@@ -16,7 +16,7 @@ FROM film AS f
 WHERE r.return_date IS NOT NULL
 ORDER BY f.title;
 
-# INTERVAL arithmetic
+-- INTERVAL arithmetic
 SELECT
 	f.title,
     INTERVAL '1' day * f.rental_duration,
@@ -27,7 +27,7 @@ FROM film AS f
 WHERE r.return_date IS NOT NULL
 ORDER BY f.title;
 
-# Calculating the expected return date
+-- Calculating the expected return date
 SELECT
     f.title,
 	r.rental_date,
@@ -39,72 +39,72 @@ FROM film AS f
     INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
 ORDER BY f.title;
 
-# Working with the current date and time
-### (1)
+-- Working with the current date and time
+-- (1)
 SELECT NOW();
 
-### (2)
+-- (2)
 SELECT CURRENT_DATE;
 
-### (3)
+-- (3)
 SELECT CAST( NOW() AS timestamp )
 
-### (4)
+-- (4)
 SELECT 
 	CURRENT_DATE,
     CAST( NOW() AS date )
 
-# Manipulating the current date and time
-### (1)
+-- Manipulating the current date and time
+-- (1)
 SELECT 
 CURRENT_TIMESTAMP::timestamp AS right_now;
 
-### (2)
+-- (2)
 SELECT
 CURRENT_TIMESTAMP::timestamp AS right_now,
 INTERVAL '5 ___' + CURRENT_TIMESTAMP AS five_days_from_now;
 
-### (3)
+-- (3)
 SELECT
 CURRENT_TIMESTAMP(0)::timestamp AS right_now,
 interval '5 days' + CURRENT_TIMESTAMP(0) AS five_days_from_now;
 
-# Using EXTRACT
-### (1)
+-- Using EXTRACT
+-- (1)
 SELECT 
   EXTRACT(dow FROM rental_date) AS dayofweek 
 FROM rental 
 LIMIT 100;
 
-### (2)
+-- (2)
 SELECT 
   EXTRACT(dow FROM rental_date) AS dayofweek, 
   COUNT(rental_id) as rentals 
 FROM rental 
 GROUP BY 1;
 
-# Using DATE_TRUNC
-### (1)
+-- Using DATE_TRUNC
+-- (1)
 SELECT DATE_TRUNC('Year', rental_date) AS rental_year
 FROM rental;
 
-### (2)
+-- (2)
 SELECT DATE_TRUNC('Month', rental_date) AS rental_month
 FROM rental;
 
-### (3)
+-- (3)
 SELECT DATE_TRUNC('Day', rental_date) AS rental_day 
 FROM rental;
 
-### (4)
+-- (4)
 SELECT 
   DATE_TRUNC('Day', rental_date) AS rental_day,
   COUNT(rental_id) AS rentals 
 FROM rental
 GROUP BY 1;
 
-# Putting it all together
-### (1)
+-- Putting it all together
+-- (1)
 SELECT 
   EXTRACT(dow FROM rental_date) AS dayofweek,
   AGE(return_date, rental_date) AS rental_days
@@ -113,7 +113,7 @@ WHERE
   rental_date BETWEEN CAST('2005-05-01' AS DATE)
    AND CAST('2005-05-01' AS DATE) + INTERVAL '90 day';
 
-### (2)
+-- (2)
 SELECT 
   c.first_name || ' ' || c.last_name AS customer_name,
   f.title,
