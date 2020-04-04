21abe4db859a9ec8c84656c529593dc4ba7f66d6
Abhee Hudani <hudaniabhee@gmail.com>
2020-04-05 01:12:54 +0530

    Chapter 4 Ccompleted

--- Functions for Manipulating Data in PostgreSQL/4.Full-text Search and PostgresSQL Extensions.sql
+++ Functions for Manipulating Data in PostgreSQL/4.Full-text Search and PostgresSQL Extensions.sql
@@ -1,30 +1,30 @@
-# A review of the LIKE operator
-### (1)
+-- A review of the LIKE operator
+-- (1)
 SELECT *
 FROM film
 WHERE title LIKE 'GOLD%';
 
-### (2)
+-- (2)
 SELECT *
 FROM film
 WHERE title LIKE '%GOLD';
 
-### (3)
+-- (3)
 SELECT *
 FROM film
 WHERE title LIKE '%GOLD%';
 
-# What is a tsvector?
+-- What is a tsvector?
 SELECT to_tsvector(description)
 FROM film;
 
-# Basic full-text search
+-- Basic full-text search
 SELECT title, description
 FROM film
 WHERE to_tsvector(title) @@ to_tsquery('elf');
 
-# User-defined data types
-### (1)
+-- User-defined data types
+-- (1)
 CREATE TYPE compass_position AS ENUM (
   	'North', 
   	'West',
@@ -32,7 +32,7 @@ CREATE TYPE compass_position AS ENUM (
   	'East'
 );
 
-### (2)
+-- (2)
 CREATE TYPE compass_position AS ENUM (
   	'North', 
   	'South',
@@ -43,19 +43,19 @@ SELECT typname
 FROM pg_type
 WHERE typname='compass_position';
 
-# Getting info about user-defined data types
-### (1)
+-- Getting info about user-defined data types
+-- (1)
 SELECT column_name, data_type, udt_name
 FROM INFORMATION_SCHEMA.COLUMNS 
 WHERE table_name ='film' and column_name='rating';
 
-### (2)
+-- (2)
 SELECT *
 FROM pg_type 
 WHERE typname='mpaa_rating'
 
-# User-defined functions in Sakila
-### (1)
+-- User-defined functions in Sakila
+-- (1)
 SELECT 
 	f.title, 
     r.rental_id, 
@@ -64,7 +64,7 @@ FROM film as f
 	INNER JOIN inventory AS i ON f.film_id=i.film_id 
     INNER JOIN rental AS r on i.inventory_id=r.inventory_id
 
-### (2)
+-- (2)
 SELECT 
 	f.title, 
     r.rental_id, 
@@ -74,7 +74,7 @@ FROM film as f
 	INNER JOIN inventory AS i ON f.film_id=i.film_id 
     INNER JOIN rental AS r on i.inventory_id=r.inventory_id
 
-### (3)
+-- (3)
 SELECT 
 	f.title, 
     r.rental_id, 
@@ -86,22 +86,22 @@ FROM film as f
 WHERE
     inventory_held_by_customer(i.inventory_id) IS NOT NULL
 
-# Enabling extensions
-### (1)
+-- Enabling extensions
+-- (1)
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
-### (2)
+-- (2)
 SELECT * 
 FROM pg_extension;
 
-# Measuring similarity between two strings
+-- Measuring similarity between two strings
 SELECT 
   title, 
   description, 
   similarity(title, description)
 FROM film
 
-# Levenshtein distance examples
+-- Levenshtein distance examples
 SELECT  
   title, 
   description, 
@@ -110,8 +110,8 @@ FROM
   film
 ORDER BY 3
 
-# Putting it all together
-### (1)
+-- Putting it all together
+-- (1)
 SELECT  
   title, 
   description 
@@ -121,7 +121,7 @@ WHERE
   to_tsvector(description) @@ 
   to_tsquery('Astounding & Drama');
 
-### (2)
+-- (2)
 SELECT 
   title, 
   description, 
